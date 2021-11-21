ca54a6c8532046e7b92537dba29b8f50dadae226
tokku5552 <physics.0121@gmail.com>
2021-11-21 15:46:17 +0900

    usernameを入れて取得できるように修正

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,24 +1,41 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import axios, { AxiosResponse } from "axios";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  username: string;
+};
 
 const Popup = () => {
   const [currentURL, setCurrentURL] = useState<string>();
+  const [username, setUsername] = useState<string>("tokku5552");
   const [currentStats, setCurrentStats] = useState<AxiosResponse>();
   const [currentTopLanguage, setCurrentTopLanguage] = useState<AxiosResponse>();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data["username"]);
+    setUsername(data["username"]);
+  });
 
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       setCurrentURL(tabs[0].url);
     });
-    const getData = async () => {
-      const stats = await getGitHubStats("tokku5552");
-      const lang = await getGitHubTopLanguage("tokku5552");
+    const getData = async (username: string) => {
+      const stats = await getGitHubStats(username);
+      const lang = await getGitHubTopLanguage(username);
       setCurrentTopLanguage(lang);
       setCurrentStats(stats);
     };
-    getData();
-  }, []);
+    getData(username);
+  }, [username]);
 
   const getGitHubStats = async (username: string) => {
     const response = await axios.get(
@@ -41,7 +58,11 @@ const Popup = () => {
       </ul>
       <div dangerouslySetInnerHTML={{ __html: currentStats?.data }} />
       <div dangerouslySetInnerHTML={{ __html: currentTopLanguage?.data }} />
-      {/* <button onClick={changeBackground}>change background</button> */}
+      <form onSubmit={onSubmit}>
+        <label>GitHub username </label>
+        <input {...register("username")} placeholder="GitHub username" />
+        <input type="submit" />
+      </form>
     </>
   );
 };
