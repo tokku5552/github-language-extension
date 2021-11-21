eba72050824b455a32cc4ea5c73c13c61d5040cd
tokku5552 <physics.0121@gmail.com>
2021-11-21 14:15:19 +0900

    add top language card

--- src/popup.tsx
+++ src/popup.tsx
@@ -5,15 +5,18 @@ import axios, { AxiosResponse } from "axios";
 const Popup = () => {
   const [currentURL, setCurrentURL] = useState<string>();
   const [currentStats, setCurrentStats] = useState<AxiosResponse>();
+  const [currentTopLanguage, setCurrentTopLanguage] = useState<AxiosResponse>();
 
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       setCurrentURL(tabs[0].url);
     });
     const getData = async () => {
-      const res = await getGitHubStats();
-      console.log(res.data);
-      setCurrentStats(res);
+      const stats = await getGitHubStats();
+      const lang = await getGitHubTopLanguage();
+      console.log(stats.data);
+      setCurrentTopLanguage(lang);
+      setCurrentStats(stats);
     };
     getData();
   }, []);
@@ -44,7 +47,7 @@ const Popup = () => {
 
   const getGitHubTopLanguage = async () => {
     const response = await axios.get(
-      "https://github-readme-stats.vercel.app/api/top-langs/?username=anuraghazra"
+      "https://github-readme-stats.vercel.app/api/top-langs/?username=anuraghazra&layout=compact"
     );
     return response;
   };
@@ -56,6 +59,7 @@ const Popup = () => {
         <li>Current Time: {new Date().toLocaleTimeString()}</li>
       </ul>
       <div dangerouslySetInnerHTML={{ __html: currentStats?.data }} />
+      <div dangerouslySetInnerHTML={{ __html: currentTopLanguage?.data }} />
       <button onClick={changeBackground}>change background</button>
     </>
   );
