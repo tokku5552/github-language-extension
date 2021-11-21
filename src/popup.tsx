54a6a93e353395f3bd7d73f15ca8a36de2c2c5ed
tokku5552 <physics.0121@gmail.com>
2021-11-21 17:04:51 +0900

    自動的にGitHubのユーザーを取得するよう修正

--- src/popup.tsx
+++ src/popup.tsx
@@ -9,7 +9,7 @@ type FormData = {
 
 const Popup = () => {
   const [currentURL, setCurrentURL] = useState<string>();
-  const [username, setUsername] = useState<string>("tokku5552");
+  const [username, setUsername] = useState<string>("");
   const [currentStats, setCurrentStats] = useState<AxiosResponse>();
   const [currentTopLanguage, setCurrentTopLanguage] = useState<AxiosResponse>();
   const {
@@ -26,21 +26,31 @@ const Popup = () => {
 
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const current = tabs[0].url as string;
       setCurrentURL(tabs[0].url);
+      const name = getGitHubUsername(current) as string;
+      setUsername(name);
+      setValue("username", name);
     });
-    const getData = async (username: string) => {
+  }, []);
+
+  useEffect(() => {
+    const fetch = async (username: string) => {
       const stats = await getGitHubStats(username);
       const lang = await getGitHubTopLanguage(username);
       setCurrentTopLanguage(lang);
       setCurrentStats(stats);
     };
-    getData(username);
+    if (username !== "") {
+      fetch(username);
+    }
   }, [username]);
 
   const getGitHubStats = async (username: string) => {
     const response = await axios.get(
       `https://github-readme-stats.vercel.app/api?username=${username}&count_private=true&show_icons=true`
     );
+    console.log(response.status);
     return response;
   };
 
@@ -48,7 +58,16 @@ const Popup = () => {
     const response = await axios.get(
       `https://github-readme-stats.vercel.app/api/top-langs/?username=${username}&layout=compact`
     );
+    console.log(response.status);
     return response;
+  };
+
+  const getGitHubUsername = (url: string) => {
+    const urlObj = new URL(url);
+    console.log(urlObj.hostname);
+    if (urlObj.hostname === "github.com") {
+      return urlObj.pathname.split("/")[1];
+    }
   };
 
   return (
@@ -56,6 +75,7 @@ const Popup = () => {
       <ul style={{ minWidth: "700px" }}>
         <li>Current URL: {currentURL}</li>
       </ul>
+
       <div dangerouslySetInnerHTML={{ __html: currentStats?.data }} />
       <div dangerouslySetInnerHTML={{ __html: currentTopLanguage?.data }} />
       <form onSubmit={onSubmit}>
