f878713f2d932634613ef6035516cb7770a6924a
tokku5552 <physics.0121@gmail.com>
2022-12-29 00:03:16 +0900

    add: jest導入

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    "roots": [
-        "src"
-    ],
-    "transform": {
-        "^.+\\.ts$": "ts-jest"
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
     },
-}; 
+  },
+  testMatch: ['**/test/**/*.test.ts'],
+};
