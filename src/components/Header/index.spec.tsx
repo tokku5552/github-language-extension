1dd1c98f86143691ac91de7b85a9500b17a6779a
tokku5552 <physics.0121@gmail.com>
2023-06-08 09:08:48 +0900

    refactor : move folder

--- src/components/Header/index.spec.tsx
+++ src/components/Header/index.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import { Header } from '.';
+import { Header } from '..';
 
 describe('Header', () => {
   test('rendering', () => {
