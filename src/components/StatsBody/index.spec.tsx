23409e5650ef381a6fdef1fc10bc1ee866c5d931
tokku5552 <physics.0121@gmail.com>
2024-10-24 23:26:20 +0900

    fix: unit test

--- src/components/StatsBody/index.spec.tsx
+++ src/components/StatsBody/index.spec.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { render } from '@testing-library/react';
 import React from 'react';
 import StatsBody from '.';
@@ -5,7 +6,12 @@ import StatsBody from '.';
 describe('StatsBody', () => {
   test('rendering', () => {
     const { container } = render(
-      <StatsBody currentStats="<p>test</p>" currentTopLanguage="<p>test</p>" />
+      <ChakraProvider>
+        <StatsBody
+          currentStats="<p>test</p>"
+          currentTopLanguage="<p>test</p>"
+        />
+      </ChakraProvider>
     );
 
     expect(container.innerHTML).toMatch('<p>test</p><p>test</p>');
