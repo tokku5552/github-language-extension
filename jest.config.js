5e006dc65fba02b7186cc894631d01fd4165659c
tokku5552 <physics.0121@gmail.com>
2025-11-20 10:20:58 +0900

    fix: revert changes

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   moduleFileExtensions: ['ts', 'tsx' ,'js'],
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsConfig: 'tsconfig.json' }],
   },
   moduleNameMapper: {
     "@/(.*)$": "<rootDir>/src/$1",
